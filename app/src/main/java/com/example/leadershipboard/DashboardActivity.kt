c7b30eddc9c8fd8016b2d505d3f8a0a18416ed9c
Roshan <roshanrahman6399@gmail.com>
2019-01-18 10:29:13 +0530

    Dashboard implementation half

---
+++ app/src/main/java/com/example/leadershipboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.leadershipboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}

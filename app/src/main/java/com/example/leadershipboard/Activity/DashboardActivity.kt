15ce0e22376bb04c78371e2743f6107e2d4ec90f
ThalapathySiva <sivaramsiva16@gmail.com>
2019-01-24 14:56:29 +0530

    logout Done.

--- app/src/main/java/com/example/leadershipboard/Activity/DashboardActivity.kt
+++ app/src/main/java/com/example/leadershipboard/Activity/DashboardActivity.kt
@@ -1,16 +1,20 @@
 package com.example.leadershipboard.Activity
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.leadershipboard.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+    private var editor: SharedPreferences.Editor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val pref = applicationContext.getSharedPreferences("MyPref", 0) // 0 - for private mode
+        editor = pref.edit()
         DataStore.tempBucket.clear()
         setOnClickListeners()
     }
@@ -22,7 +26,10 @@ class DashboardActivity : AppCompatActivity() {
             startActivity(Intent(this@DashboardActivity, AboutActivity::class.java))
         }
         materialButton.setOnClickListener {
-            startActivity(Intent(this@DashboardActivity, HelpActivity::class.java))
+            editor?.clear()
+            editor?.commit()
+            val intent = Intent(this@DashboardActivity, SignInActivity::class.java)
+            startActivity(intent)
         }
         addNewEntry.setOnClickListener {
             startActivity(Intent(this@DashboardActivity, NewEntryActivity::class.java))
